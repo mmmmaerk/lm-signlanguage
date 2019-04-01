08a75cc964aa2efc7b9c92ec14326d8bc5d06ad4
Mark Murphy <markmurphy61098@gmail.com>
2019-03-31 20:46:02 -0400

    Added documentation
    I added a little bit of documentation and got rid of the useless block of code that created a finger.

--- src/Sample.java
+++ src/Sample.java
@@ -9,16 +9,20 @@ class SampleListener extends Listener {
     public void onFrame(Controller leapController) {
         Frame frame = leapController.frame();
 
+        //creates a list of hands seen in the current frame
         HandList hands = frame.hands();
+        //creates a hand object using the first hand seen in the list of hands
         Hand firstHand = hands.get(0);
 
-        FingerList fingers = frame.fingers();
-        Finger firstFinger = fingers.get(1);
-
+        /**
+         *  this is taken straight from the leap motion example.
+         *  i believe that it's going through the list of fingers
+         *  found on the hand and creating bones of the type finger.
+         */
         for (Finger finger : firstHand.fingers()) {
             for(Bone.Type boneType : Bone.Type.values()) {
                 Bone bone = finger.bone(boneType);
-
+                //this prints the coordinates of the tip of that finger.
                 System.out.println(bone.nextJoint());
             }
         }
@@ -26,6 +30,9 @@ class SampleListener extends Listener {
     }
 }
 
+/**
+ * This was taken entirely from their Hello World example
+ */
 public class Sample {
     public static void main(String[] args) {
 
