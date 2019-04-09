d3d09152d6ba2dd666d32f482240d32c443544b0
Mark Murphy <markmurphy61098@gmail.com>
2019-04-09 11:07:37 -0400

    Add files via upload
    Added for loops to create each individual bone. There is a print statement that prints the direction of the pointer finger, but change it to whatever you want.

--- src/Sample.java
+++ src/Sample.java
@@ -14,7 +14,18 @@ class SampleListener extends Listener {
         //creates a hand object using the first hand seen in the list of hands
         Hand firstHand = hands.get(0);
         FingerList fingers = frame.fingers();
+        //create finger bone objects
+        Bone thumbFingerBone = Bone.invalid();
+        Bone pointerFingerBone = Bone.invalid();
+        Bone middleFingerBone = Bone.invalid();
+        Bone ringFingerBone = Bone.invalid();
+        Bone pinkieFingerBone = Bone.invalid();
+
+        Finger thumbFinger = fingers.get(0);
         Finger pointerFinger = fingers.get(1);
+        Finger middleFinger = fingers.get(2);
+        Finger ringFinger = fingers.get(3);
+        Finger pinkieFinger = fingers.get(4);
         /**
          *  This for loop wsa mostly taken from the Leap Motion website.
          *  It ACTUALLY tracks the tip of your pointer finger.
@@ -22,9 +33,26 @@ class SampleListener extends Listener {
          *  This one is for sure correct.
          */
         for(Bone.Type boneType : Bone.Type.values()) {
-            Bone bone = pointerFinger.bone(boneType);
-            System.out.println(bone.nextJoint());
+            thumbFingerBone = thumbFinger.bone(boneType);
         }
+
+        for(Bone.Type boneType : Bone.Type.values()) {
+            pointerFingerBone = pointerFinger.bone(boneType);
+        }
+
+        for(Bone.Type boneType : Bone.Type.values()) {
+            middleFingerBone = middleFinger.bone(boneType);
+        }
+
+        for(Bone.Type boneType : Bone.Type.values()) {
+            ringFingerBone = ringFinger.bone(boneType);
+        }
+
+        for(Bone.Type boneType : Bone.Type.values()) {
+            pinkieFingerBone = pinkieFinger.bone(boneType);
+        }
+
+        System.out.println(pointerFinger.direction());
 
     }
 }
