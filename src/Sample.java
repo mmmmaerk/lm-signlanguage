2903e0def20694be2fb3a7c4acf5f771f547b38f
Mark Murphy <markmurphy61098@gmail.com>
2019-04-25 09:57:53 -0400

    Add files via upload
    Prints the actual word spelled (A and B only)

--- src/Sample.java
+++ src/Sample.java
@@ -1,5 +1,4 @@
 import com.leapmotion.leap.*;
-
 import java.io.*;
 import java.util.ArrayList;
 
@@ -16,6 +15,7 @@ class SampleListener extends Listener {
         HandList hands = frame.hands();
         //creates a hand object using the first hand seen in the list of hands
         Hand firstHand = hands.get(0);
+        Hand secondHand = hands.get(1);
         FingerList fingers = frame.fingers();
         //create finger bone objects
         Bone thumbFingerBone = Bone.invalid();
@@ -35,13 +35,18 @@ class SampleListener extends Listener {
         boolean isD;
         boolean isJ;
         boolean isV;
+        boolean isThumbsUp;
 
         File word = new File("word.txt");
         File finalWord = new File("finalWord.txt");
 
+        char[] chars;
+        char currentLetter;
+        char prevLetter;
+        ArrayList<Integer> markers = new ArrayList<Integer>();
 
         /**
-         *  This for loop wsa mostly taken from the Leap Motion website.
+         *  This for loop was mostly taken from the Leap Motion website.
          *  It ACTUALLY tracks the tip of your pointer finger.
          *  The last version got the tips of all your fingers, I think.
          *  This one is for sure correct.
@@ -96,7 +101,7 @@ class SampleListener extends Listener {
         }
 
         if (pointerFingerBone.nextJoint().distanceTo(thumbFingerBone.nextJoint()) < 105 && pointerFingerBone.nextJoint().distanceTo(thumbFingerBone.nextJoint()) > 90) {
-            System.out.println("D");
+            //System.out.println("D");
             isD = true;
             /**
             try {
@@ -109,43 +114,78 @@ class SampleListener extends Listener {
             } **/
         }
 
-        if ((firstHand.rotationProbability(frame) == 1) && (pinkieFingerBone.nextJoint().distanceTo(thumbFingerBone.nextJoint()) > 60)) {
-            System.out.println("J");
-            isJ = true;
-        }
-
         if (pointerFingerBone.nextJoint().distanceTo(middleFingerBone.nextJoint()) > 85 &&
                 pointerFingerBone.nextJoint().distanceTo(middleFingerBone.nextJoint()) < 110 &&
                 pinkieFingerBone.nextJoint().distanceTo(middleFingerBone.nextJoint()) > 35 &&
                 pinkieFingerBone.nextJoint().distanceTo(middleFingerBone.nextJoint()) < 50) {
 
-                isV = true;
-                System.out.println("V");
+            isV = true;
+            System.out.println("V");
+            try {
+                FileWriter writer = new FileWriter(word, true);
+                writer.write("V");
+                writer.flush();
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (thumbFingerBone.nextJoint().distanceTo(pinkieFingerBone.nextJoint()) > 100 &&
+                (thumbFingerBone.nextJoint().distanceTo(pinkieFingerBone.nextJoint()) < 110) &&
+                (pointerFingerBone.nextJoint().distanceTo(pinkieFingerBone.nextJoint()) > 35) &&
+                (pointerFingerBone.nextJoint().distanceTo(pinkieFingerBone.nextJoint())) < 45) {
+            isThumbsUp = true;
+            System.out.println("Finished");
+            leapController.setPaused(true);
+            if (leapController.isPaused()) {
+                try {
+                    FileReader reader = new FileReader(word);
+                    chars = new char[(int) word.length()];
+                    reader.read(chars);
+
+                    //adds markers based on the letters in chars[]
+                    markers.add(0);
+                    for (int i = 1; i < chars.length; i++) {
+                        currentLetter = chars[i];
+                        prevLetter = chars[i -1];
+
+                        if (currentLetter != prevLetter) {
+                            markers.add(i);
+                        }
+                    }
+                    //appends the characters found at the markers to finalWord.txt
+                    BufferedWriter writer = new BufferedWriter(new FileWriter("finalWord.txt", true));
+                    for (int i = 0; i <markers.size(); i++) {
+                        System.out.print(chars[markers.get(i)]);
+                        try {
+                            writer.write(markers.get(i));
+                            writer.flush();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    writer.close();
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
 
         }
 
     }
 }
 
-/**
- * This was taken entirely from their Hello World example
- */
 public class Sample {
     public static void main(String[] args) {
         Controller leapController = new Controller();
         SampleListener leapListener = new SampleListener();
-
-        char[] chars;
-        char currentLetter;
-        char prevLetter;
-        ArrayList<Integer> markers = new ArrayList<Integer>();
-        File word = new File("word.txt");
-        File finalWord = new File("finalWord.txt");
-
+        leapController.setPolicy(Controller.PolicyFlag.POLICY_ALLOW_PAUSE_RESUME);
+        leapController.setPaused(false);
         leapController.addListener(leapListener);
 
-        //run until enter is pressed
-        System.out.println("Press Enter to quit");
+        File word = new File("word.txt");
 
         //makes word.txt blank
         try {
@@ -156,37 +196,10 @@ public class Sample {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //adds markers based on word.txt
-        try {
-            FileReader reader = new FileReader(word);
-            chars = new char[(int) word.length()];
-            reader.read(chars);
 
-            markers.add(0);
-            for (int i = 1; i < chars.length; i++) {
-                currentLetter = chars[i];
-                prevLetter = chars[i -1];
+        //run until enter is pressed
+        System.out.println("Press Enter to quit");
 
-                if (currentLetter != prevLetter) {
-                    markers.add(i);
-                }
-            }
-            //appends the characters found at the markers to finalWord.txt
-            BufferedWriter writer = new BufferedWriter(new FileWriter("finalWord.txt", true));
-            System.out.print(chars);
-            for (int i = 0; i <markers.size(); i++) {
-                try {
-                    writer.write(markers.get(i));
-                    writer.flush();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            writer.close();
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         try {
             System.in.read();
         } catch (IOException e) {
